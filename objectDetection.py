34b7c03d0ac1fd37c6be06945598075bfb800809
smanerik <shravan.manerikar@visteon.com>
2022-01-08 18:32:41 +0530

    Segregated and added comments

--- objectDetection.py
+++ objectDetection.py
@@ -2,12 +2,15 @@ import cv2
 import numpy as np
 import time
 
-cap = cv2.VideoCapture("52M27S_1640863347.mp4")
+# to stream in from a webcam
 # cap = cv2.VideoCapture(-1)
 
+# default constant 
+# width and height for converting each frame into the blob
 width = 320
 height = 320
 
+# confidence and nms threshold value to find out max confidence among a given set
 CONFIDENCE_THRESHOLD = 0.5
 NMS_THRESHOLD = 0.3         #lower the value more aggressive the working (i.e less number of boxes)
 
@@ -18,23 +21,33 @@ prev_frame_time = 0
  
 # used to record the time at which we processed current frame
 new_frame_time = 0
- 
+
+# list to store classifier names
 classNames = []
-with open(classFile, 'rt') as f:
-    classNames = f.read().strip('\n').split('\n')
 
-# print(classNames)
-
+# configuration and weight file paths for the respective yolov3 network
+# yolov3-320 model
 modelConfiguration = 'misc/yolov3.cfg'
 modelWeights = 'misc/yolov3.weights'
 
+# yolov3-tiny model
 # modelConfiguration = 'misc/yolov3-tiny.cfg'
 # modelWeights = 'misc/yolov3-tiny.weights'
 
-net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+# to stream in from a video
+cap = cv2.VideoCapture("52M27S_1640863347.mp4")
+ 
+# open the file and read the class names 
+with open(classFile, 'rt') as f:
+    classNames = f.read().strip('\n').split('\n')
 
+# create neural network using the model configurations and
+# set the different targets
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+# function to find the objects in an image
 def findObjects(outputs, image):
     oHeight, oWidth, oCenter = image.shape
     boundingBox = []
@@ -54,7 +67,6 @@ def findObjects(outputs, image):
                 classIds.append(classId)
                 confidenceLevels.append(float(confidenceLevel))
 
-    # print(len(boundingBox))
     indices = cv2.dnn.NMSBoxes(boundingBox, confidenceLevels, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
     print(indices)
 
@@ -66,6 +78,7 @@ def findObjects(outputs, image):
         cv2.putText(image, f'{classNames[classIds[i]].upper()} {int(confidenceLevels[i]*100)}%',
                     (x, y-h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(255, 0, 255), 2)
 
+# loop till user inputs a q
 while True:
     success, img = cap.read()
 
@@ -76,7 +89,6 @@ while True:
     new_frame_time = time.time()
 
     # Calculating the fps
- 
     # fps will be number of frame processed in given time frame
     # since their will be most of time error of 0.001 second
     # we will be subtracting it to get more accurate result
@@ -97,19 +109,14 @@ while True:
     net.setInput(blob)
 
     totalLayers = net.getLayerNames()
-    # print(type(net.getUnconnectedOutLayers()))
-   
-    outputLayers = [totalLayers[i-1] for i in net.getUnconnectedOutLayers()] 
-    # print(outputLayers)
 
+    outputLayers = [totalLayers[i-1] for i in net.getUnconnectedOutLayers()] 
     outputs = net.forward(outputLayers)
-    # print(len(outputs))
-    # print(outputs[0].shape)
-    # print(outputs[1].shape)
-    # print(outputs[2].shape)
-    # print(outputs[0][0])
 
     findObjects(outputs, img)
 
     cv2.imshow('Image', img)
-    cv2.waitKey(1)
+
+    # if user inputs q, break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
